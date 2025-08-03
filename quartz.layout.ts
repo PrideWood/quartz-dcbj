67fccca76fb6cf21ffd1c56bdfbba96773f31e97
Wesley <boquanchai@gmail.com>
2025-08-03 14:39:35 +0800

    remove MOC on Mobiles

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -42,7 +42,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
+    // Component.Graph(),
+    Component.DesktopOnly(Component.Graph()),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
