d4fcac42b553977cbf4246c9f2b8aa0685c5aa1e
Wesley <boquanchai@gmail.com>
2025-08-08 17:45:27 +0800

    align icon

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "✒ DCBJ",
+    pageTitle: "DCBJ",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
